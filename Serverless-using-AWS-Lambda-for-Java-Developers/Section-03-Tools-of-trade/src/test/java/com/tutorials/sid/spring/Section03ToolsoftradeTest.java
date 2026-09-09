bc194b720b4b293868a8f20e935266109efa7d0e
javawithinfinitepossibilities@gmail.com <javawithinfinitepossibilities@gmail.com>
2026-09-09 17:29:25 +0530

    Section-03

---
+++ Serverless-using-AWS-Lambda-for-Java-Developers/Section-03-Tools-of-trade/src/test/java/com/tutorials/sid/spring/Section03ToolsoftradeTest.java
@@ -0,0 +1,15 @@
+package com.tutorials.sid.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author kunmu On 09-09-2026
+ */
+@SpringBootTest(classes = Section03Toolsoftrade.class)
+class Section03ToolsoftradeTest {
+    @Test
+    void contextLoads() {
+        System.out.println("Spring Boot Application Started Successfully");
+    }
+}
